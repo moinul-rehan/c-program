08e5d8b55b0591d1bbe789c013f4f70704dd1e98
moinul-rehan <mir281310@gmail.com>
2023-01-10 00:34:35 +0600

    first commit

---
+++ test.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int n, i;
+
+    printf("Enter the rang:");
+    scanf("%d", &n);
+
+    for(i=1; i<=n; i++)
+    {
+        printf("%d . Moinul Islam Rehan\n", i);
+    }
+return 0;
+}
